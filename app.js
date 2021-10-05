c6a708b98cd99b48eb143ebd9240a548eb559944
unknown <devik.kota@gmail.com>
2021-10-05 23:38:50 +0530

    router started

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ import fetch from "node-fetch";
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import _ from "lodash";
 
 // const express = require("express");
 // const bodyParser = require("body-parser");
 // const ejs = require("ejs");
 // const https = require("https");
 
+//All api data
+
+let latest_data;
+let politics_data;
+let business_data;
+let sports_data;
+let science_data;
+let technology_data;
+let startup_data;
+let entertainment_data;
+let automobile_data;
+
+
+//Js render scripts
 
 const app = express();
 app.set("view engine", "ejs");
@@ -22,7 +37,7 @@ app.get("/", (req, res)=>{
 app.get("/:category_name", (req, res)=>{
     const category = req.params.category_name;
     console.log(category);
-    res.render("news", {});
+    res.render("news", {ejs_section_name : _.startCase(_.toLower(category))});
 })
 
 
@@ -33,18 +48,42 @@ app.listen(4000, ()=>{
 
 //Fetching data
 
-const url = "https://inshortsapi.vercel.app/news?category=national";
-fetch(url)
-.then((res)=>{
-    return res.json();
-})
-.then((data)=>{
-    console.log(data);
-})
-.catch((err)=>{
-    console.log(err);
-})
+var fetched_data = []; 
+const urls = ["https://inshortsapi.vercel.app/news?category=national"];
+let link = "https://inshortsapi.vercel.app/news?category=";
 
+urls.push(link + "politics");
+urls.push(link + "business");
+urls.push(link + "sports");
+urls.push(link + "science");
+urls.push(link + "technology");
+urls.push(link + "startup");
+urls.push(link + "entertainment");
+urls.push(link + "automobile");
+
+
+const all_category = ["latest_data", "politics_data", "business_data", "sports_data", "science_data", 
+                "technology_data", "startup_data", "entertainment_data", "automobile_data"];
+
+for(var i=0; i<9; i++){
+    fetch(urls[i])
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+
+        fetched_data.push(data)
+        console.log(fetched_data);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+};
+
+
+
+
+console.log(latest_data);
 
 //API -------------------------------------------------------------------------------------------------------------------------
 // https://inshortsapi.vercel.app/news?category=all
